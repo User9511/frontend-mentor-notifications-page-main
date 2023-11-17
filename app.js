170322f419beb42beb29686c42a03f4983978ac6
Smgy94 <Shanemcgeown@gmail.com>
2023-11-17 23:06:45 +0000

    Set Up Project Structure
    HTML:
    - Semantic Structure: Employed semantic HTML elements, including <nav>, <main>, <section>, and <h3>, to establish a meaningful and structured layout for the notifications page.
    
    - Accessible Navigation: Implemented a navigation bar with clear headings and a button for marking all notifications as read, ensuring a user-friendly experience for both mouse and keyboard users.
    
    - Role and ARIA Attributes: Assigned relevant roles and ARIA attributes to enhance accessibility, making the page more user-friendly for individuals relying on screen readers and assistive technologies.
    
    CSS:
    
    - Responsive Design: Utilized media queries to create a responsive layout, ensuring an optimal viewing experience across various devices, particularly focusing on screens up to 768px wide.
    
    - Consistent Styling: Maintained a cohesive color scheme and typography throughout the page, contributing to a visually pleasing and user-friendly design.
    
     - Focus Visibility: Enhanced focus visibility through clear outlines for clickable elements, improving the navigational experience for users relying on keyboard or screen readers.
    
    Accessibility:
    
    - Keyboard Accessibility: Ensured all interactive elements are accessible via keyboard input, facilitating navigation and interaction without reliance on a mouse.
    
    - Testing with Accessibility Tools: Conducted thorough testing using accessibility tools and validators to ensure compliance with accessibility standards, promoting inclusivity and usability for all users.

---
+++ app.js
@@ -0,0 +1,57 @@
+let notificationNumber = document.getElementById("notification-number");
+let notifications = 3;
+let btnMarkRead = document.getElementById("mark-read");
+let unreadIcon = document.querySelectorAll(".unread-icon");
+let notificationContainer = document.querySelectorAll(".notification");
+notificationNumber.innerHTML = `${notifications}`;
+
+// Mark all notifications as read
+function markAllAsRead() {
+  btnMarkRead.addEventListener("click", () => {
+    notificationNumber.innerHTML = "0";
+
+    unreadIcon.forEach((icon) => {
+      icon.style.display = "none";
+    });
+
+    notificationContainer.forEach((unread) => {
+      unread.classList.remove("unread");
+    });
+  });
+}
+
+markAllAsRead();
+
+// Function to update each notification count
+let notificationCounter = () => {
+  notificationNumber.innerHTML = `${notifications}`;
+};
+
+notificationContainer.forEach((notification, index) => {
+  let handleClick = (e) => {
+    if (e.type === "click" || (e.type === "keydown" && (e.key === "" || e.key === "Enter"))) {
+      if (notification.classList.contains("unread") && notifications > 0) {
+        notifications--;
+        notification.classList.remove("unread");
+
+        const clickedIcon = unreadIcon[index];
+        if (clickedIcon) {
+          clickedIcon.classList.remove("unread-icon");
+        }
+
+        notificationCounter();
+        notification.removeEventListener("click", handleClick);
+      }
+    }
+  };
+
+  notification.addEventListener("click", handleClick);
+
+  // Add keydown event listener to handle space and enter keys
+  notification.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      // Trigger the click event for space and enter keys
+      notification.click();
+    }
+  });
+});
